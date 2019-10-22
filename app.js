06ca31be5df892be17ddd610c101d0ca611cf8d2
merjench <merjen.chary@gmail.com>
2019-10-22 15:47:53 -0400

    more package testing

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-// nodejs.org documentation
+// nodejs.org for documentation + npmjs.com for packages
+const validator = require('validator') // require installed package name
+const chalk = require('chalk')
 const getNotes = require('./notes.js')  // can be named anything you would like
 
 const msg = getNotes()
-
 console.log(msg)
+
+// console.log(validator.isURL('https//google.com'))
+console.log(chalk.green('Success'))
